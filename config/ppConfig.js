8c765e229b46b920d07ce829333835a5ce4fb85c
Idilahmed <idil@mac-books-MacBook-Pro.local>
2019-11-03 21:40:33 -0800

    ppconfig3

--- config/ppConfig.js
+++ config/ppConfig.js
@@ -32,7 +32,7 @@ const User = require('../models/user');
 passport.use(new FacebookStrategy({
    clientID: process.env.FACEBOOK_APP_ID,
    clientSecret: process.env.FACEBOOK_APP_SECRET,
-   callbackURL: "https://kause/herokuapp.com/auth/facebook/callback",
+   callbackURL: "https://kause.herokuapp.com/auth/facebook/callback/",
    enableProof: true
    },
    // function (accessToken, refreshToken, profile, cb) {
